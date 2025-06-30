45623e82633e121ab5ab99c0236bfd9c2c4fbff2
VeroxUniverse <stauplays@gmail.com>
2025-06-30 07:13:38 +0200

    Fixed Diving Helmet

---
+++ src/main/java/com/silopante/createair/Createair.java
@@ -0,0 +1,12 @@
+package com.silopante.createair;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(value="createair")
+public class Createair {
+    public static final String MOD_ID = "createair";
+    public static final Logger LOGGER = LogUtils.getLogger();
+}
+
---
+++ src/main/java/com/silopante/createair/mixins/CreateAirQualityFixMixin.java
@@ -0,0 +1,34 @@
+package com.silopante.createair.mixins;
+
+import com.simibubi.create.content.equipment.armor.BacktankUtil;
+import com.simibubi.create.content.equipment.armor.DivingHelmetItem;
+import fuzs.thinair.helper.AirQualityHelperImpl;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+
+@Mixin(AirQualityHelperImpl.class)
+public abstract class CreateAirQualityFixMixin {
+
+    @Inject(method = "isSensitiveToAirQuality", at = @At("HEAD"),
+            cancellable = true,
+            remap = false
+    )
+    private void overrideAirSensitivity(LivingEntity ent, CallbackInfoReturnable<Boolean> cir) {
+
+        if (!(ent instanceof Player player) || player.isCreative() || player.isSpectator()) return;
+        if (DivingHelmetItem.isWornBy(player)) {
+            List<ItemStack> backtanks = BacktankUtil.getAllWithAir(player);
+
+            if (!backtanks.isEmpty() && DivingHelmetItem.isWornBy(player)) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
